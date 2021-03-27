2447aa0539ba88aee5e0ce402d5ff61f9ed14870
Kilian Schuster <0.duraaa@gmail.com>
2021-03-27 23:40:04 +0100

    Design app structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import "antd/dist/antd.css";
 import reportWebVitals from './reportWebVitals';
+import { Layout, Input, Slider, Space, Card, Button } from 'antd';
+import { StarSliders } from './components/StarSliders'
+import { ModsSelection } from './components/ModsSelection';
+import { TeamMode } from './components/TeamMode';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const { Content, Sider } = Layout;
+
+class App extends React.Component {
+    state = {
+        buttonLoading: false,
+        buttonText: "Start game"
+    }
+
+    onClick = () => {
+        this.setState({
+            buttonLoading: true,
+            buttonText: "Validating inputs..."
+        });
+        if (!window.lobbyName.state.value) {
+            return;
+        }
+        this.setState({buttonLoading: false});
+    }
+
+    render() {
+        const { buttonLoading, buttonText } = this.state;
+
+        return (
+
+            <Layout className="layout">
+                <Sider width={200} className="site-layout-background">
+                </Sider>
+                <Content style={{ padding: '0 50px' }}>
+                    <div className="site-layout-content">
+                        <Space direction="vertical" style={{ width: "100%" }}>
+                            <Card title="Lobby name">
+                                <Input ref={(r) => window.lobbyName = r} />
+                            </Card>
+                            <Card title="Max Players">
+                                <Slider ref={(r) => window.maxPlayers = r}
+                                    min={1}
+                                    max={16}
+                                />
+                            </Card>
+                            <Card title="Star difficults restriction">
+                                <StarSliders ref={(r) => window.starSliders = r} />
+                            </Card>
+                            <Card title="Mods">
+                                <ModsSelection ref={(r) => window.mods = r} />
+                            </Card>
+                            <Card title="Team Mode">
+                                <TeamMode ref={(r) => window.teamMode = r} />
+                            </Card>
+                            <Button type="primary" onClick={this.onClick} loading={buttonLoading}>{buttonText}</Button>
+                        </Space>
+                    </div>
+                </Content>
+            </Layout>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
